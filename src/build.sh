b64f79b4867a9d1709f8bcc87aef189f6bb01f36
James Swineson <github@public.swineson.me>
2018-10-20 15:15:27 +0000

    somewhat a patch system

--- src/build.sh
+++ src/build.sh
@@ -12,6 +12,14 @@ IMG_MOUNT_OFFSET=4194304
 #######################################################################################
 
 
+function foreach() {
+	FILE=$1
+	COMMAND="${@:2}"
+	while read -u 10 p; do
+		$COMMAND "$p";
+	done 10<"$1"
+}
+
 function chrootdo() {
 	chroot "${BUILD_BINARIESDIRECTORY}/golden_image/rootfs" "$@"
 }
@@ -41,6 +49,37 @@ function mount_rootfs() {
 	mount -o loop,offset=${IMG_MOUNT_OFFSET} "${BUILD_BINARIESDIRECTORY}/golden_image/"*.img "${IMG_MOUNT_POINT}"
 }
 
+function apply_changeset() {
+	CHANGESET="${BUILD_SOURCESDIRECTORY}/$1"
+
+	echo "Running pre apply hook..."
+	! ( "${CHANGESET}/hooks/pre_apply_changeset.sh" )
+
+	echo "Applying packages..."
+	foreach "${CHANGESET}/packages/remove.list" chrootdo apt-get purge -y
+	chrootdo apt-get update -y
+	chrootdo apt-get full-upgrade -y
+	foreach "${CHANGESET}/packages/install.list" chrootdo apt-get install -y
+	chrootdo rm -rf /var/lib/apt/lists
+	chrootdo mkdir -p /var/lib/apt/lists
+
+	echo "Applying rootfs..."
+	if [ -d "${CHANGESET}/rootfs" ]; then
+		cp -rfv "${CHANGESET}/rootfs" "${IMG_MOUNT_POINT}"
+	fi
+
+	echo "Running post apply hook..."
+	! ( "${CHANGESET}/hooks/post_apply_changeset.sh" )
+
+}
+
+function umount_rootfs() {
+	echo "Unmounting rootfs..."
+	umount "${IMG_MOUNT_POINT}"
+}
+
+
+
 #######################################################################################
 # workflow
 #######################################################################################
@@ -52,4 +91,6 @@ fi
 unzip_image
 check_image
 mount_rootfs
+apply_changeset changeset_common
+umount_rootfs
 
