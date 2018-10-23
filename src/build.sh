c92be1ba4c956f30eadc12b96ec37ed49a757a08
James Swineson <github@public.swineson.me>
2018-10-23 13:48:08 +0800

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -13,9 +13,8 @@ export LC_ALL="C.UTF-8"
 export LC_MESSAGES="C.UTF-8"
 export LANG="C.UTF-8"
 
-
 #######################################################################################
-# helpers
+# basic checks
 #######################################################################################
 
 if [[ $EUID -ne 0 ]]; then
@@ -31,9 +30,15 @@ else
 	CROSSBUILD=0
 fi
 
+#######################################################################################
+# helpers
+#######################################################################################
+
 RED='\033[0;31m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
+
+alias rm_safer=rm --one-file-system
 
 function print_stage() {
 	echo -e "${RED}${@}${NC}"
@@ -42,6 +47,16 @@ function print_stage() {
 function print_info() {
         echo -e "${YELLOW}${@}${NC}"
 
+}
+
+function retry() {
+	n=0
+	until [ $n -ge 3 ]
+	do
+		"$@" && break
+		n=$[$n+1]
+		sleep 1
+	done
 }
 
 function foreach() {
@@ -86,13 +101,13 @@ function systemctl-mask() {
 #######################################################################################
 
 function reset_build_dirs() {
-	rm -rf --one-file-system "${BUILD_ARTIFACTSTAGINGDIRECTORY}/*"
-	rm -rf --one-file-system "${BUILD_BINARIESDIRECTORY}/*"
+	rm_safer -rf "${BUILD_ARTIFACTSTAGINGDIRECTORY}/*"
+	rm_safer -rf "${BUILD_BINARIESDIRECTORY}/*"
 }
 
 function download_image() {
 	print_stage "Downloading golden image..."
-	wget "${GOLDEN_IMAGE_URL}" -O "${GOLDEN_IMAGE}"
+	retry wget -c -O "${GOLDEN_IMAGE}" "${GOLDEN_IMAGE_URL}"
 }
 
 function unzip_image() {
@@ -138,12 +153,12 @@ function apply_changeset() {
 	foreach "${CHANGESET}/packages/remove.list" apt-compat purge -y
 	set -e
 	apt-compat autoremove --purge -y
-	apt-compat update -y
+	retry apt-compat update -y
 	apt-compat full-upgrade -y
-	foreach "${CHANGESET}/packages/install.list" apt-compat install -y
+	foreach "${CHANGESET}/packages/install.list" apt-compat install -y --no-install-recommends
 	apt-compat autoremove --purge -y
 	apt-compat clean -y
-	rm -rf "${IMG_MOUNT_POINT}/var/lib/apt/lists/"*
+	rm_safer -rf "${IMG_MOUNT_POINT}/var/lib/apt/lists/"*
 
 	print_info "Applying rootfs..."
 	if [ -d "${CHANGESET}/rootfs" ]; then
@@ -195,7 +210,7 @@ function generate_readonly_image() {
 	SQUASHFS_NAME="system.squashfs"
 
 	print_info "Making the new root..."
-	rm -rf "${NEWIMGROOT}"
+	rm_safer -rf "${NEWIMGROOT}"
 	mkdir -p "${NEWIMGROOT}/${PATHPREFIX}"
 	cp -rv "${IMG_MOUNT_POINT}/boot" "${NEWIMGROOT}"
 	mksquashfs "${IMG_MOUNT_POINT}" "${NEWIMGROOT}/${PATHPREFIX}/${SQUASHFS_NAME}" -comp xz -Xbcj arm -info
@@ -204,7 +219,7 @@ function generate_readonly_image() {
 	cp -r "${BUILD_BINARIESDIRECTORY}/golden_image/"*.img ${NEWIMG}
 	mkdir -p "${NEWIMG_MOUNT_POINT}"
 	mount -o loop,offset=${IMG_MOUNT_OFFSET} "${NEWIMG}" "${NEWIMG_MOUNT_POINT}"
-	rm -rf --one-file-system "${NEWIMG_MOUNT_POINT}"/*
+	rm_safer -rf "${NEWIMG_MOUNT_POINT}"/*
 	mv "${NEWIMGROOT}"/* "${NEWIMG_MOUNT_POINT}"
 	umount "${NEWIMG_MOUNT_POINT}"
 
@@ -221,8 +236,8 @@ function generate_readonly_image() {
 	xz --compress --force --format=xz --check=sha256 -1 --threads=0 --verbose "${NEWIMG}"
 
 	print_info "Cleaning up..."
-	rm -rf "${NEWIMGROOT}"
-	rm -rf "${NEWIMG_MOUNT_POINT}"
+	rm_safer -rf "${NEWIMGROOT}"
+	rm_safer -rf "${NEWIMG_MOUNT_POINT}"
 }
 
 #######################################################################################
