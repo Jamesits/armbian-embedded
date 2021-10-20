ba9a251147e2bff717e556e7be8ec33f9e8d3f96
James Swineson <github@public.swineson.me>
2021-10-20 23:25:53 +0800

    update package from dhcpcd to dhcpcd5

--- userpatches/customize-image.sh
+++ userpatches/customize-image.sh
@@ -90,7 +90,7 @@ UniversalCommands() {
 		wireguard-tools \
 		iw wpasupplicant wireless-tools wireless-regdb hostapd \
 		gcc-9-base jq wget cracklib-runtime
-	apt-get install -y openvpn squashfs-tools dhcpcd live-boot
+	apt-get install -y openvpn squashfs-tools dhcpcd5 live-boot
 
 	# systemd units
 	systemctl disable ssh.service
